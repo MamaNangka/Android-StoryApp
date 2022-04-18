45f3dea6b41c6e96136ed9c5982b6b445727cfa7
Ryan Chandra <ryan.chandra173@gmail.com>
2022-04-18 17:06:01 +0700

    Initial commit

--- app/src/main/java/com/rchdr/myapplication/view/LoginActivity.kt
+++ app/src/main/java/com/rchdr/myapplication/view/LoginActivity.kt
@@ -1,12 +1,25 @@
 package com.rchdr.myapplication.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.rchdr.myapplication.R
+import com.rchdr.myapplication.databinding.ActivityLoginBinding
+
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var LoginBinding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        LoginBinding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(LoginBinding.root)
+
+        LoginBinding.loginTv2.setOnClickListener {
+            Intent(this@LoginActivity, RegisterActivity::class.java).also {
+                startActivity(it)
+            }
+        }
+
     }
 }
--- app/src/main/java/com/rchdr/myapplication/view/RegisterActivity.kt
+++ app/src/main/java/com/rchdr/myapplication/view/RegisterActivity.kt
@@ -1,12 +1,25 @@
 package com.rchdr.myapplication.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.rchdr.myapplication.R
+import com.rchdr.myapplication.databinding.ActivityLoginBinding
+import com.rchdr.myapplication.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var RegisterBinding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        RegisterBinding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(RegisterBinding.root)
+
+        RegisterBinding.registerTv2.setOnClickListener {
+            Intent(this@RegisterActivity, LoginActivity::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
